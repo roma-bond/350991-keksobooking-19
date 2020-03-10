56d6a6e33f0729aaddabedfbe58cba3e4b55928e
roma-bond <bondarenkore@gmail.com>
2020-03-10 02:54:55 -0300

    Исправления 2

--- js/map.js
+++ js/map.js
@@ -9,8 +9,6 @@
   var MAP_Y_MIN = 130;
   var MAP_Y_MAX = 630;
   var ENTER_KEY = 'Enter';
-  var PRICE_LOW = 10000;
-  var PRICE_MIDDLE = 50000;
 
   var mapElement = document.querySelector('.map');
   var mapMainPinElement = document.querySelector('.map__pin--main');
@@ -18,17 +16,23 @@
   var defaultMainPinLeftOffset = mapMainPinElement.offsetLeft;
   var mapFiltersForm = mapElement.querySelector('.map__filters');
   var filterFieldsets = document.querySelectorAll('.map__filters fieldset, .map__filters select');
-  var housingTypeInput = mapElement.querySelector('#housing-type');
-  var selectedHousingType = housingTypeInput.value;
-  var housingPriceInput = mapElement.querySelector('#housing-price');
-  var selectedHousingPrice = housingPriceInput.value;
-  var housingRoomsInput = mapElement.querySelector('#housing-rooms');
-  var selectedHousingRooms = housingRoomsInput.value;
-  var housingGuestsInput = mapElement.querySelector('#housing-guests');
-  var selectedHousingGuests = housingGuestsInput.value;
+  var filterElements = Array.from(filterFieldsets);
   var pageActive = false;
   var ads = [];
-  var checkedFeatures = [];
+  var priceMap = {
+    'low': {
+      min: 0,
+      max: 10000
+    },
+    'middle': {
+      min: 10000,
+      max: 50000
+    },
+    'high': {
+      min: 50000,
+      max: Infinity
+    }
+  };
 
   var getAddressCoordinates = function () {
     var x = mapMainPinElement.offsetLeft + PIN_WIDTH / 2;
@@ -144,104 +148,46 @@
     }
   };
 
-  var verifyPrice = function (price, filterValue) {
-    var match;
-    switch (filterValue) {
-      case 'any':
-        match = true;
-        break;
-      case 'low':
-        match = (price <= PRICE_LOW);
-        break;
-      case 'middle':
-        match = (price <= PRICE_MIDDLE);
-        break;
-      case 'high':
-        match = (price > PRICE_MIDDLE);
-        break;
-    }
-    return match;
-  };
+  var filterRules = {
+    'housing-type': function (data, filter) {
+      return filter.value === data.offer.type;
+    },
+    'housing-price': function (data, filter) {
+      return data.offer.price >= priceMap[filter.value].min && data.offer.price < priceMap[filter.value].max;
+    },
+    'housing-rooms': function (data, filter) {
+      return filter.value === data.offer.rooms.toString();
+    },
+    'housing-guests': function (data, filter) {
+      return filter.value === data.offer.guests.toString();
+    },
+    'housing-features': function (data, filter) {
+      var checkListElements = Array.from(filter.querySelectorAll('input[type=checkbox]:checked'));
 
-  var verifyFeatures = function (adFeatures) {
-    var tempCheckedFeatures = checkedFeatures.slice();
-    for (var i = 0; i < adFeatures.length; i++) {
-      if (tempCheckedFeatures.length > 0) {
-        for (var j = 0; j < tempCheckedFeatures.length; j++) {
-          if (adFeatures[i] === tempCheckedFeatures[j]) {
-            tempCheckedFeatures.splice(j, 1);
-            break;
-          }
-        }
-      } else {
-        break;
-      }
+      return checkListElements.every(function (it) {
+        return data.offer.features.some(function (feature) {
+          return feature === it.value;
+        });
+      });
     }
-    return (tempCheckedFeatures.length === 0);
   };
 
   var applyFilters = function (list) {
-    var filteredList = [];
-    if (list) {
-      filteredList = list.filter(function (ad) {
-        return ((selectedHousingType === 'any') || (ad.offer.type === selectedHousingType));
-      }).filter(function (ad) {
-        return verifyPrice(ad.offer.price, selectedHousingPrice);
-      }).filter(function (ad) {
-        return ((selectedHousingRooms === 'any') || (ad.offer.rooms === parseInt(selectedHousingRooms, 10)));
-      }).filter(function (ad) {
-        return ((selectedHousingGuests === 'any') || (ad.offer.guests === parseInt(selectedHousingGuests, 10)));
-      }).filter(function (ad) {
-        return verifyFeatures(ad.offer.features);
+    return list.filter(function (item) {
+      return filterElements.every(function (filter) {
+        return (filter.value === 'any') ? true : filterRules[filter.id](item, filter);
       });
-    }
-    return filteredList;
+    });
   };
 
-  var onHousingTypeChange = window.data.debounce(function (evt) {
-    selectedHousingType = evt.target.value;
+  var onFiltersChange = function () {
     window.pin.renderPins(applyFilters(window.map.ads));
-  });
-
-  var onHousingPriceChange = window.data.debounce(function (evt) {
-    selectedHousingPrice = evt.target.value;
-    window.pin.renderPins(applyFilters(window.map.ads));
-  });
-
-  var onHousingRoomsChange = window.data.debounce(function (evt) {
-    selectedHousingRooms = evt.target.value;
-    window.pin.renderPins(applyFilters(window.map.ads));
-  });
-
-  var onHousingGuestsChange = window.data.debounce(function (evt) {
-    selectedHousingGuests = evt.target.value;
-    window.pin.renderPins(applyFilters(window.map.ads));
-  });
-
-  var onHousingFeaturesChange = window.data.debounce(function (evt) {
-    if (evt.target.checked) {
-      checkedFeatures.push(evt.target.value);
-    } else {
-      checkedFeatures.splice(checkedFeatures.indexOf(evt.target.value), 1);
-    }
-    window.pin.renderPins(applyFilters(window.map.ads));
-  });
-
-  var onFiltersChange = function (evt) {
-    var filterType = evt.target;
-    switch (filterType) {
-      case housingTypeInput: onHousingTypeChange(evt); break;
-      case housingPriceInput: onHousingPriceChange(evt); break;
-      case housingRoomsInput: onHousingRoomsChange(evt); break;
-      case housingGuestsInput: onHousingGuestsChange(evt); break;
-      default: onHousingFeaturesChange(evt); break;
-    }
   };
 
   mapMainPinElement.addEventListener('mousedown', onMainPinMousedown);
   mapMainPinElement.addEventListener('keydown', onMainPinHitEnter);
 
-  mapFiltersForm.addEventListener('change', onFiltersChange);
+  mapFiltersForm.addEventListener('change', window.data.debounce(onFiltersChange));
 
   window.map = {
     MAP_X_MIN: MAP_X_MIN,
